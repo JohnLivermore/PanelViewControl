28f77f41b2f0d018062a3225037dc205f058fa30
John Livermore <jlivermore@bigwavesoftware.com>
2019-06-28 17:55:10 -0500

    initial commit

---
+++ PanelViewControl/PanelViewControl/MainPageModel.cs
@@ -0,0 +1,31 @@
+﻿using FreshMvvm;
+using Xamarin.Forms;
+
+namespace PanelViewControl
+{
+    public class MainPageModel : FreshBasePageModel
+    {
+        public Command ShowPanelView
+        {
+            get
+            {
+                return new Command(() =>
+                {
+                    PanelViewVisible = true;
+                });
+            }
+        }
+
+        public bool PanelViewVisible { get; set; }
+
+        public Command PanelShowing
+        {
+            get
+            {
+                return new Command(() =>
+                {
+                });
+            }
+        }
+    }
+}
---
+++ PanelViewControl/PanelViewControl/PanelView.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PanelViewControl
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PanelView : ContentView
+	{
+		public PanelView ()
+		{
+			InitializeComponent ();
+		}
+
+        public static readonly BindableProperty PanelVisibleProperty = BindableProperty.Create(
+                                propertyName: nameof(PanelVisible),
+                                returnType: typeof(bool),
+                                declaringType: typeof(PanelView),
+                                defaultBindingMode: BindingMode.OneWay,
+                                defaultValue: false,
+                                propertyChanged: (bindable, oldValue, newValue) =>
+                                {
+                                    if ((bool)newValue)
+                                    {
+                                        ((PanelView)bindable).Showing.Execute(true);
+                                    }
+                                });
+
+        public bool PanelVisible
+        {
+            get { return (bool)base.GetValue(PanelVisibleProperty); }
+            set { base.SetValue(PanelVisibleProperty, value); }
+        }
+
+        public static readonly BindableProperty ShowingProperty = BindableProperty.Create(
+                                    propertyName: nameof(Showing),
+                                    returnType: typeof(Command),
+                                    declaringType: typeof(PanelView),
+                                    defaultBindingMode: BindingMode.OneWay);
+
+        public Command Showing
+        {
+            get { return (Command)base.GetValue(ShowingProperty); }
+            set { base.SetValue(ShowingProperty, value); }
+        }
+    }
+}
