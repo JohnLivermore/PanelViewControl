28df7e1b28342210a2de98c0b35ff70f93251b5a
John Livermore <jlivermore@bigwavesoftware.com>
2019-06-29 10:05:44 -0500

    fixed MainPage to bind to correct property, and now the PanelView fires an event when its IsVisible property turns from false to true

--- PanelViewControl/PanelViewControl/MainPageModel.cs
+++ PanelViewControl/PanelViewControl/MainPageModel.cs
@@ -1,22 +1,31 @@
 ﻿using FreshMvvm;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using Xamarin.Forms;
 
 namespace PanelViewControl
 {
-    public class MainPageModel : FreshBasePageModel
+    public class MainPageModel : FreshBasePageModel, INotifyPropertyChanged
     {
+        public new event PropertyChangedEventHandler PropertyChanged;
+
         public Command ShowPanelView
         {
             get
             {
                 return new Command(() =>
                 {
-                    PanelViewVisible = true;
+                    PanelViewVisible = !PanelViewVisible;
                 });
             }
         }
-
-        public bool PanelViewVisible { get; set; }
+        bool _panelViewVisible;
+        public bool PanelViewVisible
+        {
+            set { SetProperty(ref _panelViewVisible, value); }
+            get { return _panelViewVisible; }
+        }
 
         public Command PanelShowing
         {
@@ -26,6 +35,21 @@ namespace PanelViewControl
                 {
                 });
             }
+        }
+
+        bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Object.Equals(storage, value))
+                return false;
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
